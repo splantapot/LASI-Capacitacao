5b442dc1157ca97a948892e8e7c48fa8664e65c4
splantapot <joaovictor1007@outlook.com.br>
2026-06-12 02:23:17 -0300

    Add: Menu da calculadora

--- 01-calculadora/calculadora/console.py
+++ 01-calculadora/calculadora/console.py
@@ -1,11 +1,14 @@
 import subprocess, os
+from typing import List
 
-def cls():
+# Função de limpeza
+def cls() -> None:
     """Limpar o console."""
     subprocess.run("cls", shell=True)
     # Para linux:
     # subprocess.run("clear", shell=True)
 
+# Funções para escrever colorido
 def prRed(s): print("\033[91m {}\033[00m".format(s))
 def prGreen(s): print("\033[92m {}\033[00m".format(s))
 def prYellow(s): print("\033[93m {}\033[00m".format(s))
@@ -14,3 +17,31 @@ def prPurple(s): print("\033[95m {}\033[00m".format(s))
 def prCyan(s): print("\033[96m {}\033[00m".format(s))
 def prLightGray(s): print("\033[97m {}\033[00m".format(s))
 def prBlack(s): print("\033[90m {}\033[00m".format(s))  # Corrected from 98 to 90 (standard ANSI)
+
+# Escrever Título Formatado
+def escrever_titulo(titulo:str, tam_linha:int = 20) -> None:
+    """Escreve um título no formato indicado em uma linha de tamanho especificado."""
+    # tam_linha: Tamanho da linha de um título
+    espaco = int((tam_linha-len(titulo))/2)     # Espaçamento para centralizar o título
+    print("="*tam_linha)
+    print(" "*espaco+titulo)        
+    print("="*tam_linha)
+
+# Escrever opções
+def solicitar_opcao(opcoes:List[str] = []) -> int:
+    """Escreve opções de uma lista. O índice 0 do array será a opção 1.
+    Retorna: valor int da opção selecionada.
+    Se retornar -1, houve 'erro: opção não listada'
+    Se retornar -2, houve 'erro: opção digitada não foi um número'
+    """
+    for (i, opcao) in enumerate(opcoes):
+        print(f"{i+1}. {opcao}")
+    resposta = input("Insira uma opção: ")
+    try:
+        resposta = int(resposta)                        # Tenta converter para int
+        if (resposta <= 0 or resposta > len(opcoes)):   # Caso o número respondido seja maior que as opções
+            return -1                                   # Erro de opção não listada
+        return resposta                                 # Retorna a resposta (se válida)
+    except ValueError:
+        return -2   # Erro de conversão
+    
--- 01-calculadora/main.py
+++ 01-calculadora/main.py
@@ -1,8 +1,47 @@
 from calculadora.console import *
+from typing import List
+
+executando = True   # Variável de controle
+
+def encerrar():
+    global executando
+    executando = False
+
+opcoes = [
+    ["Somar", lambda _: _],
+    ["Subtrair", lambda _: _],
+    ["Multiplicar", lambda _: _],
+    ["Dividir", lambda _: _],
+    ["Projetar Contador Sincrono", lambda _: _],
+    ["Sair", encerrar],
+]
 
 def main():
-    cls()
-    print("Hello World!")
+    try:
+        while executando:
+            # Aguarda resposta válida
+            cls()
+            resposta_valida = False
+            while not resposta_valida:
+                escrever_titulo("Menu")
+                opcao = solicitar_opcao([txt for txt, func in opcoes])
+                if (opcao == -1):   #Erro: Operação inválida
+                    cls()
+                    prRed("Erro: Opção não definida no menu.")
+                    continue
+                elif (opcao == -2): #Erro: Resposta inválida
+                    cls()
+                    prRed("Erro: Insira uma resposta válida (int).")
+                    continue
+                resposta_valida = True
+            # Executa ação correspondente
+            opcoes[opcao-1][1]()
+
+    except KeyboardInterrupt:
+        prCyan("\n*Interrupção gerada pelo usuário.")
+    finally:
+        # Fim
+        print("\nVolte sempre!!")
     
 if __name__ == "__main__":
     main()
