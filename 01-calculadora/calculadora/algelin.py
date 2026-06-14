cdf3ed9b9c62e67bcc40237f898c5aa0c1defdbf
splantapot <joaovictor1007@outlook.com.br>
2026-06-14 17:37:58 -0300

    Add: FIM - Menu Sistema Linear

--- 01-calculadora/calculadora/algelin.py
+++ 01-calculadora/calculadora/algelin.py
@@ -52,11 +52,11 @@ def matriz_transposta(matriz:List[List]) -> List[List]:
     ]
     return nova_matriz
 
-def matriz_cofatores(matriz:List[List]) -> List[List]:
+def matriz_cofatores(matriz:List[List]) -> Tuple[List[List], int]:
     """Retorna a matriz dos cofatores da matriz dada. Retorna um Tuple: (valor, erro)"""
     ordem = len(matriz)
-    if (ordem == 0): return (0, -1)              # Retorna erro -1 se a matriz estiver vazia
-    if (ordem != len(matriz[0])): return (0, -2) # Retorna erro -2 se a matriz não for quadrada
+    if (ordem == 0): return ([], -1)              # Retorna erro -1 se a matriz estiver vazia
+    if (ordem != len(matriz[0])): return ([], -2) # Retorna erro -2 se a matriz não for quadrada
     
     # Prossegue para o cálculo
 
@@ -95,15 +95,10 @@ def produto_matricial(matrizA:List[List], matrizB:List[List]) -> Tuple[List[List
                 nova_matriz[i][j] += matrizA[i][k]*matrizB[k][j]
     return (nova_matriz, 0)
 
-def produto_matriz_escalar(matriz:List[List], escalar:float) -> List[List]:
+def produto_matriz_escalar(matriz:List[List[float]], escalar:float) -> List[List]:
     """Realiza o produto de uma matriz por um escalar. Retorna a matriz resultante, ou uma matriz vazia em caso de erro."""
     if (not matriz): return []
-    m = len(matriz)
-    for i in range(m):
-        n = len(matriz[i])
-        for j in range(n):
-            matriz[i][j] *= escalar
-    return matriz
+    return [[elemento * escalar for elemento in linha] for linha in matriz]
 
 # =========================================================
 # Funções de interação com o usuário
@@ -111,6 +106,10 @@ def produto_matriz_escalar(matriz:List[List], escalar:float) -> List[List]:
 
 def solicitar_entrada_sistema() -> None:
     """Executa a resolução do sistema linear."""
+    
+    ordem:int = 0  # Ordem do sistema (número de variáveis/equações)
+    i_exec:int = 0  # Variável para controlar execução (Usei para controlar o menu e a matriz exibida)
+    err_atribuicao = False # Variável que indica erro de atribuição
 
     def construir_secao() -> None:
         cls()
@@ -118,20 +117,80 @@ def solicitar_entrada_sistema() -> None:
         escrever_titulo("Modo: Sistema Linear Quadrado", 100)
         prLightGray("Esse modo serve para calcular a resolução de um Sistema Linear Quadrado.")
         prLightGray("-> (número de icógnitas igual ao número de equações)")
+        if (ordem >= 1):
+            prGreen(f"Ordem: {ordem}")
+            for i in range(ordem):
+                prGreen(f"{matriz[i]}     {matriz_resultado[i]}")
+        elif (i_exec != 0):
+            prRed(f"*A ordem inserida não é válida ({i_exec}).")
+        if err_atribuicao:
+            prRed("*O valor inserido não foi válido.")
 
-    variaveis:List[str] = []
-    var_validas:bool = False
-
-    while not var_validas:
+    # Solicita ordem e atualiza a matriz
+    while not ordem >= 1:
         construir_secao()
-        print("Insira as variáveis (espaçadas por /) ou deixe em branco para inserir a matriz dos coeficientes:")
-        texto = input(">> ")
-        variaveis = texto.split('/')
-        # Verifica se alguma variável é inválida
-        var_validas = True
-        for v in variaveis:
-            if (not v.isalpha()): var_validas = False
+        i_exec += 1 #Aqui, indica quantas vezes o menu apareceu.
+        print("Insira a ordem do sistema:")
+        try:
+            ordem = int(input(">> "))
+        except ValueError:
+            ordem = -1
         
+    matriz:List[List[float]] = [[0 for _ in range(ordem)] for _ in range(ordem)]
+    matriz_resultado:List[List[float]] = [[0] for _ in range(ordem)] # Matriz resultado
 
+    i_exec = 0
+    while i_exec < ordem**2:
+        construir_secao()
+        err_atribuicao = False
+        j = i_exec%ordem
+        i = i_exec//ordem
+        print(f"Insira um valor (aceita float) para a posição A[{i}][{j}]:")
+        try:
+            matriz[i][j] = float(input(">> ").replace(',', '.'))
+        except ValueError:
+            err_atribuicao = True
+        else:
+            i_exec += 1 # Incrementa em 1 se o valor foi atribuído corretamente
+
+    i_exec = 0
+    while i_exec < ordem:
+        construir_secao()
+        err_atribuicao = False
+        i = i_exec%ordem
+        print(f"Insira um valor (aceita float) para a posição R[{i}][0]:")
+        try:
+            matriz_resultado[i][0] = float(input(">> ").replace(',', '.'))
+        except ValueError:
+            err_atribuicao = True
+        else:
+            i_exec += 1 # Incrementa em 1 se o valor foi atribuído corretamente
+
+    construir_secao()
+    print("Confirmar matriz? Insira 'r' para redigitar valores.")
+    text = input(">> ").lower().replace(' ', '')
+    if (text == "r"): 
+        # Comentado, pois a minha função de menu já executa em loop
+        # solicitar_entrada_sistema()
+        return # Quebra a execução
+
+    # Cálculo do sistema
+    construir_secao()
+    det, err = determinante(matriz)
+    if (det == 0):
+        prYellow("Sistema indeterminado: Determinante igual a 0.")
+    else:
+        prYellow(f"Determinante: {det}")
+        try:
+            inv_det = 1 / det
+            matriz_cof, err = matriz_cofatores(matriz)
+            matriz_adjunta = matriz_transposta(matriz_cof)
+            matriz_inversa = (produto_matriz_escalar(matriz_adjunta, inv_det))
+            matriz_final, err = produto_matricial(matriz_inversa, matriz_resultado)
+            prYellow("Matriz resultado (x, y, z ...):")
+            for linha in matriz_final:
+                prYellow(linha)
+        except TypeError as e:
+            print(e)
 
     input("Tecle 'Enter' para repetir...")
